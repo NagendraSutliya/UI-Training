c54da993435230b919e8717d92e543e90eeb88ed
NagendraSutliya <108868292+NagendraSutliya@users.noreply.github.com>
2022-12-20 14:41:28 +0530

    updated code

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -18,28 +18,28 @@ function playRound(playerSelection, computerSelection) {
         return("Computer Win! Paper beats Rock");
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         playerScore++;
-        console.log(playerScore);
+        document.write(playerScore);
         return ('Player win! Rock beats Scissors');
         
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         playerScore++;
-        console.log(playerScore);
+        document.write(playerScore);
         return ('Player Win! Paper beats Rock');
         
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         computerScore++;
-        console.log(computerScore);
+        document.write(computerScore);
         return ('Computer Win! Scissors beats Paper');
         
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         computerScore++;
-        console.log(computerScore);
+        document.write(computerScore);
         return ('Computer Win! Rock beats Scissors');
         
     } else (playerSelection == 'scissors' && computerSelection == 'paper') 
     {
         playerScore++;
-        console.log(playerScore);
+        document.write(playerScore);
         return('Player Win! Scissors beats Paper');
         
     } 
@@ -50,7 +50,7 @@ function game(playerSelection) {
     let player = playerSelection;
     let computer = computerSelection;
    
-    console.log(playRound(player, computer));
+    document.write(playRound(player, computer));
     
     if (playerScore == 5) {
         return ('The player has defeated computer!');
@@ -63,7 +63,7 @@ function game(playerSelection) {
 let playerSelection = prompt("Enter your choice").toLowerCase();
 let computerSelection = getComputerChoice();
 
-console.log(playerSelection);
-console.log(computerSelection);
+document.write(playerSelection + "<br/>");
+document.write(computerSelection + "<br/>");
 
-console.log(game(playerSelection));
+game(playerSelection);
